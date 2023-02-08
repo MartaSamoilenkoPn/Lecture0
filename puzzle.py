564a2a46d9b589fb0cfcc79d032e9ffc813ffe73
MartaSamoilenkoPn <samoilenko.pn@ucu.edu.ua>
2023-02-08 09:49:14 +0200

    validate doctests

--- puzzle.py
+++ puzzle.py
@@ -6,5 +6,26 @@ link
 """
 
 def validate_board(board : list) -> bool:
-
+    """
+    >>> validate_board([
+ "**** ****",
+ "***1 ****",
+ "**  3****",
+ "* 4 1****",
+ "     9 5 ",
+ " 6  83  *",
+ "3   1  **",
+ "  8  2***",
+ "  2  ****"
+])
+    False
+    >>> validate_board([])
+    False
+    >>> validate_board([1])
+    True
+    """
     return None
+
+if __name__ == "__main__" :
+    import doctest
+    doctest.testmod(verbose=True)
